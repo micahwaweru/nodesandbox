8cd671c20f1871bed3b7554da7a802277b912344
micahwaweru <micah.waweru@gmail.com>
2022-07-25 10:02:53 -0500

    im annoyed

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer"
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+
+
+
